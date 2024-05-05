7959b95e2c406463c22d40d9761d87c433055e14
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2024-05-05 14:07:54 +0200

    fix: friendly names for test sets

--- test/Normal/williams_tests.jl
+++ test/Normal/williams_tests.jl
@@ -10,36 +10,45 @@ end
 
 @testitem "mean(::ClosedWilliamsProduct, p::Logpdf{Laplace}, q::Normal)" begin
     include("normal_utils.jl")
-    using FiniteDifferences
     using SpecialFunctions
     rng = StableRNG(123)
+
     for _ in 1:10
-        μ_1, σ = rand(rng)*10, rand(rng)*10
-        μ_2, θ = μ_1, rand(rng)*10
-        func_var = (x) -> mean(ClosedFormExpectation(), Logpdf(Laplace(μ_2, θ)), Normal(μ_1, x))
-        
-        @testset "ClosedFormExpectation gradient is ClosedWilliamsProduct" begin
-            williams_result = mean(ClosedWilliamsProduct(), Logpdf(Laplace(μ_2, θ)), Normal(μ_1, σ))
-            @test central_fdm(5, 1)(func_var, σ) ≈ williams_result[2]
+        @testset "μ and loc are equal" begin
+            μ_1, σ = rand(rng)*10, rand(rng)*10
+            θ = rand(rng)*10
+            williams_result = mean(ClosedWilliamsProduct(), Logpdf(Laplace(μ_1, θ)), Normal(μ_1, σ))
             @test -1/(2*θ) ≈ williams_result[1]
+            @test williams_result[2] ≈ -2/(sqrt(2π)*θ)
         end
+
+        @testset "μ = loc ± σ" begin
+            μ_2, θ = rand(rng)*10, rand(rng)*10
+            σ = rand(rng)*10
         
-        @testset "Some comparison with known results" begin
-            
             williams_result_right = mean(ClosedWilliamsProduct(), Logpdf(Laplace(μ_2, 1/2)), Normal(μ_2 + σ, σ))
             @test -1 + sqrt(2/(MathConstants.e * π)) - erf(1/sqrt(2)) ≈ williams_result_right[1]
 
             williams_result_left = mean(ClosedWilliamsProduct(), Logpdf(Laplace(μ_2, 1/2)), Normal(μ_2 - σ, σ))
             @test -1 - sqrt(2/(MathConstants.e * π)) + erf(1/sqrt(2)) ≈ williams_result_left[1]
 
-            @test (williams_result_right[1] + williams_result_left[1]) ≈ -2
+            @test williams_result_right[2] ≈ williams_result_left[2]
+        end
+
+        @testset "μ ≠ loc, loc = 0" begin
+            
+            μ_1, σ = rand(rng)*10, rand(rng)*10
+            θ = rand(rng)*10
 
             centered_laplace = Laplace(0, θ)
-            right_normal = Normal(μ_2, σ)
-            left_normal  = Normal(-μ_2, σ)
+            right_normal = Normal(μ_1, σ)
+            left_normal  = Normal(-μ_1, σ)
 
-            @test (mean(ClosedWilliamsProduct(), Logpdf(centered_laplace), right_normal) + mean(ClosedWilliamsProduct(), Logpdf(centered_laplace), left_normal))[1] ≈ -1/θ
-            @test mean(ClosedWilliamsProduct(), Logpdf(centered_laplace), right_normal)[2] ≈ mean(ClosedWilliamsProduct(), Logpdf(centered_laplace), left_normal)[2]
+            williams_result_left = mean(ClosedWilliamsProduct(), Logpdf(centered_laplace), left_normal)
+            williams_result_right = mean(ClosedWilliamsProduct(), Logpdf(centered_laplace), right_normal)
+
+            @test williams_result_left[1] + williams_result_right[1] ≈ -1/θ
+            @test williams_result_left[2] ≈ williams_result_right[2]
         end
     end
 end
