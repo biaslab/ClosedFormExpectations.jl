794ebfb8a8cd84067c5b75d5994fac4bf8bdc181
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2024-05-06 12:08:03 +0200

    fix: support GaussianDistributionsFamily for Logpdf param in ClosedWilliamsProduct

--- src/Normal/williams.jl
+++ src/Normal/williams.jl
@@ -1,18 +1,19 @@
-import Distributions: Normal
+import Distributions: Normal, std
+import ExponentialFamily: GaussianDistributionsFamily
 using StaticArrays
 using SpecialFunctions: erfc, erf
 
-function mean(::ClosedWilliamsProduct, p::Logpdf{Normal{T}}, q::Normal) where {T}
-    μ_q, σ_q = q.μ, q.σ
-    μ_p, σ_p = p.dist.μ, p.dist.σ
+function mean(::ClosedWilliamsProduct, p::Logpdf{NormalType}, q::Normal{T}) where {T, NormalType <: GaussianDistributionsFamily}
+    μ_q, σ_q = mean(q), std(q)
+    μ_p, σ_p = mean(p.dist), std(p.dist)
     return @SVector [
         -(μ_p - μ_q)/σ_p^2,
         -σ_q/σ_p^2
     ]
 end
 
-function mean(::ClosedWilliamsProduct, p::Logpdf{Laplace{T}}, q::Normal) where {T}
-    μ, σ = q.μ, q.σ
+function mean(::ClosedWilliamsProduct, p::Logpdf{Laplace{T}}, q::Normal{T}) where {T}
+    μ, σ = mean(q), std(q)
     (loc, θ) = params(p.dist)
     diff = loc - μ
     exp_part = exp(-diff^2/(2*σ^2))
