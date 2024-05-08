c569ce94a47b78cfa32c3aca477835059ff0cd3a
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2024-05-08 14:52:27 +0200

    fix: include interface in main file

--- src/support_exponential_family.jl
+++
@@ -1,6 +0,0 @@
-import ExponentialFamily: ExponentialFamilyDistribution
-
-function mean(expectation::ClosedFormExpectation, f, q::ExponentialFamilyDistribution)
-    dist = convert(Distribution, q)
-    return mean(expectation, f, dist)
-end
