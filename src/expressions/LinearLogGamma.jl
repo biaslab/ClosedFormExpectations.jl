888c47cae6909187739ecb38826ffa45ffda989c
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2024-05-31 14:03:57 +0200

    docs: add LinearLogGamma docs

--- src/expressions/LinearLogGamma.jl
+++ src/expressions/LinearLogGamma.jl
@@ -4,6 +4,22 @@ using LinearAlgebra
 
 import Distributions: ContinuousMultivariateDistribution, params
 
+"""
+    LinearLogGamma(α, β, weights)
+
+An unnormalized multivariate distribution derived from the LogGamma distribution. (see LogGamma)
+
+The LinearLogGamma distribution is an distribution on a multidimensional `x`, derived from the LogGamma distribution. It is defined as:
+$\widetilde{\mathcal{LG}}(x \mid \alpha, \beta, w) = \mathcal{LG}(x^{\top} w \mid a, b)$, 
+where weights is a fixed vector of covariates, and \alpha and \beta are the scale and shape parameters of the LogGamma distribution, respectively.
+Fields
+
+α::T: The scale parameter of the LogGamma distribution.
+
+β::T: The shape parameter of the LogGamma distribution.
+
+weights::C: The fixed vector of covariates.
+"""
 struct LinearLogGamma{T<:Real, C <: AbstractVector} <: ContinuousMultivariateDistribution
     α::T
     β::T
